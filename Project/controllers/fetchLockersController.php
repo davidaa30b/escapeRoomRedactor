8f0053995fe3926aea981e7142f676701118466d
David <97000892+davidaa30b@users.noreply.github.com>
2024-06-27 19:02:19 +0300

    Delete Project directory

--- Project/controllers/fetchLockersController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../models/Database.php";
-
-$database = new Database();
-$lockers = $database->getAllLockers();
-header('Content-Type: application/json'); // Ensure the response is JSON
-
-echo json_encode($lockers);
